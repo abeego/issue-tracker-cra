00d8ec7e7d76beef24178c343226c44bbf35b88c
merima.begovic <karic.merima@hotmail.com>
2018-05-02 21:53:56 +0200

    Added SASS support to CRA

--- issue-tracker/src/App.js
+++ issue-tracker/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import './app.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+const Home = () => {
+  return <div>Home</div>;
 }
+
+const App = () => (
+  <Router>
+    <Route path='/' component={Home} />
+  </Router>
+)
 
 export default App;
